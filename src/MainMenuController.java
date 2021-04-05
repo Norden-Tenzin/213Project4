59e35bc73985b3d4bc98c49bc95e56cdb1a70457
Vedant <vm.WorkAddress@gmail.com>
2021-04-05 10:20:56 -0400

    added UI

---
+++ src/MainMenuController.java
@@ -0,0 +1,18 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainMenuController {
+
+
+    @FXML
+    void OrderCoffee(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OrderDoughnuts(ActionEvent event) {
+
+    }
+}
