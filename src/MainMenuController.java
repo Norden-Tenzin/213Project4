a6fa4b2a949061dc76219b90397c3e684b0ec234
Vedant <vm.WorkAddress@gmail.com>
2021-04-05 11:35:50 -0400

    added doughnuts UI

--- src/MainMenuController.java
+++ src/MainMenuController.java
@@ -1,18 +1,35 @@
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
 
 public class MainMenuController {
 
 
-    @FXML
-    void OrderCoffee(ActionEvent event) {
+   //  @FXML
+    public void OrderCoffee(ActionEvent event) {
 
+     
     }
 
-    @FXML
-    void OrderDoughnuts(ActionEvent event) {
+   //  @FXML
+    public void OrderDoughnuts(ActionEvent event) throws IOException {
+      FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Doughnuts.fxml"));
+      Parent root1 = (Parent) fxmlLoader.load();
+      Stage stage = new Stage();
+      stage.setTitle("Order Donuts");
+      stage.setResizable(false);
+      stage.setScene(new Scene(root1));
 
+      //After the window is open, make main menu disabled
+      stage.initModality(Modality.APPLICATION_MODAL);
+      stage.showAndWait();
     }
 }
